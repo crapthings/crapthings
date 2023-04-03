4922bec6024b1e1471b5c24fde7531248c775988
crapthings <crapthings@gmail.com>
2023-04-03 16:49:44 +0800

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -2,29 +2,36 @@
 
 # Install Docker
 
-sudo apt-get update
+function install () {
+  sudo apt-get update
 
-sudo apt-get install -y \
+  sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg
-    
-sudo mkdir -m 0755 -p /etc/apt/keyrings
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  sudo mkdir -m 0755 -p /etc/apt/keyrings
 
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-sudo apt-get update
+  echo \
+    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo apt-get update
 
-clear
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+}
 
-echo "docker installed"
+function checkDockerVersion () {
+  clear
 
-docker --version
-docker compose version
+  echo "docker installed"
+
+  docker --version
+  docker compose version
+}
+
+install
+checkDockerVersion

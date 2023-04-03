a5c665cc62b0f94c4cda14b22bebeca50a3df581
crapthings <crapthings@gmail.com>
2023-04-03 16:28:54 +0800

    Create install-portainer.sh

---
+++ install-portainer.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Install Portainer
+
+# Prompt the user to enter a port number
+echo "Please enter a port number (default: 9443): "
+
+# Read the user input
+read port
+
+# Display the entered port number
+echo "The entered port number is: $port"
+
+if [ -z "$port" ]; then
+  port=9443
+fi
+
+docker volume create portainer_data
+docker run -d -p $port:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+docker ps
